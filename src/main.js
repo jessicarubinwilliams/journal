59d505a0aeae169e172662df082123035e5aca14
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-02-28 17:23:03 -0500

    link dependencies and export and import Entry and Journal objects
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Journal from './journal.js';
+import Entry from './entry.js';
